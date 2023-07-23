57b3f48452508d56eec0152ce2e3f1ac49f3e934
xdaly <mohamedaliezzeddine1@gmail.com>
2023-07-23 16:37:14 +0100

    .

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -2,16 +2,21 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GeneralService } from 'src/app/services/general.service';
 import { RouterModule } from '@angular/router';
+import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [CommonModule,RouterModule],
+  imports: [CommonModule,RouterModule,NgbAccordionModule,NgbCollapseModule],
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent {
   isOpened: any;
+
+
+  public consultation  : boolean = false
   constructor(public GeneralService: GeneralService) {
     this.GeneralService.sideBarOpened.subscribe((isOpened:boolean)=>{
       this.isOpened = isOpened
